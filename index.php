5abf056a845a2bf67ab194c2ec9060fd6b5a872a
Tawanda Kembo <tkembo@gmail.com>
2013-06-22 11:46:32 +0200

    removed uncessary code

--- index.php
+++ index.php
@@ -13,14 +13,6 @@ $app_config = array (
 try {
 	// Construct biNu object
 	$binu_app = new biNu_app($app_config);
-
-	if (TESTING) {
-		// Show test info
-		add_test_info();
-
-		/* Override TTL for testing purposes */
-		$binu_app->time_to_live = 1;
-	}
 	
 	$binu_app->add_style( array('name' => 'body_text', 'color' => '#1540eb') );
 	$binu_app->add_text('Hello world', 'body_text');
@@ -36,13 +28,5 @@ try {
 	app_error('Error: '.$e->getMessage());
 }
 
-/* Test function declarations */
-function add_test_info() {
-	global $$binu_app;
-	$binu_app->add_text('Your device id is :'.$binu_app->device_id, 'body');
-	$binu_app->add_text('Your Width is :'.$binu_app->screen_width, 'body');
-	$binu_app->add_text('Your Height is :'.$binu_app->screen_height, 'body');
-	$binu_app->add_text('Your Orientation is :'.$binu_app->orientation, 'body');
-}
 
 ?>
